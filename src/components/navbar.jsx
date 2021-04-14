28c37115815a3bc7661e95d0d326570153185c64
Muhammad Jahanzaib <66225603+Zaibi727@users.noreply.github.com>
2021-04-14 15:48:57 +0500

    Add files via upload

---
+++ src/components/navbar.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+
+
+class NavBar extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <nav className="navbar navbar-light bg-light">
+                    <a className="navbar-brand" href="#">
+                        Navbar <span className="badge badge-pill badge-secondary">
+                        {this.props.totalCounters}
+                        </span>
+                    </a>
+                </nav>
+            </div>
+         );
+    }
+}
+ 
+export default NavBar;
